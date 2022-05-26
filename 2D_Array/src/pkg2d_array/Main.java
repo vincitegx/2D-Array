d0e3f3a58fda92e3cf9064e8c400d6cd2af7f318
David Tega <40434274+vincitegx@users.noreply.github.com>
2022-05-26 02:11:17 +0100

    Add files via upload

---
+++ 2D_Array/src/pkg2d_array/Main.java
@@ -0,0 +1,17 @@
+
+package pkg2d_array;
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
